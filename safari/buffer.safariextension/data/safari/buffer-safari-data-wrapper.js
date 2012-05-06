3d73c87513923519049eb1e8997d42a730b2f0c3
Tom Ashworth <tom@phuu.net>
2012-05-06 12:43:41 +0100

    2.1.0 - Up to date with Chrome, uses Extender.

---
+++ safari/buffer.safariextension/data/safari/buffer-safari-data-wrapper.js
@@ -0,0 +1,19 @@
+var DataWrapper = function () {
+
+    var config = {};
+    config.endpoint = {
+        http: 'http://static.bufferapp.com/images/extensions/',
+        https: 'https://d389zggrogs7qo.cloudfront.net/images/extensions/'
+    };
+    
+    return {
+        get: function (file) {
+            file = file.replace(/data\//, '');
+            return (document.location.protocol === 'http:' ? config.endpoint.http : config.endpoint.https) + file;
+        }
+    }
+    
+};
+
+if(!xt) var xt = {};
+xt.data = DataWrapper();
