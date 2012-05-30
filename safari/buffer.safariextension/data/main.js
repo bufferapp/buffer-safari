058ba2edae7bbc9ca73fc5b1146194bb98022bbd
Tom Ashworth <tom@phuu.net>
2012-05-30 15:34:24 +0100

    2.2: Deployable, I believe. Please test.

--- safari/buffer.safariextension/data/main.js
+++ safari/buffer.safariextension/data/main.js
@@ -12,7 +12,7 @@ var config = {};
 config.plugin = {
     label: "Buffer This Page",
     guide: 'http://bufferapp.com/guides/safari/installed',
-    version: "2.1.7",
+    version: "2.2",
     menu: {
         page: {
             label: "Buffer This Page"
