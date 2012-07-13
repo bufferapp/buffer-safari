548b33b598ae1050ae90673301737c09f73b3764
Tom Ashworth <tom@phuu.net>
2012-07-13 15:54:40 +0300

    Add default options from Chrome to allow all extensions to use the same submodule code.

--- safari/buffer.safariextension/data/safari/buffer-safari-data-wrapper.js
+++ safari/buffer.safariextension/data/safari/buffer-safari-data-wrapper.js
@@ -11,9 +11,11 @@ var DataWrapper = function () {
             file = file.replace(/data\/shared\//, '');
             return (document.location.protocol === 'http:' ? config.endpoint.http : config.endpoint.https) + file;
         }
-    }
+    };
     
 };
 
 if(!xt) var xt = {};
 xt.data = DataWrapper();
+
+xt.options = {"buffer.op":"true","buffer.op.facebook":"facebook","buffer.op.hacker":"true","buffer.op.key-combo":"alt+b","buffer.op.key-enable":"key-enable","buffer.op.reader":"reader","buffer.op.reddit":"reddit","buffer.op.twitter":"twitter","buffer.run":"true"};
