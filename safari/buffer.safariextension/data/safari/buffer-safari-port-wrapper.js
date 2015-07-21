ece14d62fb7c655d86c99d7a0a3110835ea7f9d8
Dan Farrelly <daniel.j.farrelly@gmail.com>
2015-07-21 12:34:05 +0000

    Whitespace changes

--- safari/buffer.safariextension/data/safari/buffer-safari-port-wrapper.js
+++ safari/buffer.safariextension/data/safari/buffer-safari-port-wrapper.js
@@ -3,41 +3,42 @@
  * Firefox ports. The Safari API changes slightly depending on whether this
  * is loaded in a contentScript or not.
  *
- * port is either a safari tab object or the global safari.self object in 
+ * port is either a safari tab object or the global safari.self object in
  * content scripts
  */
 var PortWrapper = function (port, name) {
-    
-    var contentScript = false;
-    if( port.tab ) contentScript = true;
-    
-    return {
-        on: function (type, cb) {
-            //console.log(name, "listening for", type);
-            return port.addEventListener("message", function (ev) {
-                if( ev.name == type ) cb(ev.message);
-            }, false);
-        },
-        off: function (type, cb) {
-            port.removeEventListener(type, cb);
-        },
-        emit: function(type, payload) {
-            //console.log(name, "dispatching ", type, payload);
-            if( contentScript ) port.tab.dispatchMessage(type, payload);
-            else port.page.dispatchMessage(type, payload);
-        },
-        destroy: function () {
-            port = null;
-        },
-        name: port.name,
-        raw: port
-    };
-    
+
+  var contentScript = false;
+  if( port.tab ) contentScript = true;
+
+  return {
+    on: function (type, cb) {
+      //console.log(name, "listening for", type);
+      return port.addEventListener("message", function (ev) {
+        if( ev.name == type ) cb(ev.message);
+      }, false);
+    },
+    off: function (type, cb) {
+      // NOTE - this does not work
+      port.removeEventListener(type, cb);
+    },
+    emit: function(type, payload) {
+      //console.log(name, "dispatching ", type, payload);
+      if( contentScript ) port.tab.dispatchMessage(type, payload);
+      else port.page.dispatchMessage(type, payload);
+    },
+    destroy: function () {
+      port = null;
+    },
+    name: port.name,
+    raw: port
+  };
+
 };
 
 if( !xt ) var xt = {};
 xt.port = {
-    on: function () {console.log("port.on called before initialised.", arguments)},
-    off: function () {console.log("port.off called before initialised.", arguments)},
-    emit: function () {console.log("port.emit called before initialised.", arguments)}
+  on: function () {console.log("port.on called before initialised.", arguments)},
+  off: function () {console.log("port.off called before initialised.", arguments)},
+  emit: function () {console.log("port.emit called before initialised.", arguments)}
 };
