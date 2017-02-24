381547e37357414aafdae10e8614692eabc139a1
Philippe Masset <pioul06+github@gmail.com>
2017-02-24 18:56:17 +0100

    Abstract away internal url generation for compat w/ Safari

--- safari/buffer.safariextension/data/safari/buffer-safari-data-wrapper.js
+++ safari/buffer.safariextension/data/safari/buffer-safari-data-wrapper.js
@@ -1,19 +1,11 @@
-var DataWrapper = function () {
-
-    var config = {};
-    config.endpoint = {
-        http: 'http://static.bufferapp.com/images/extensions/',
-        https: 'https://d389zggrogs7qo.cloudfront.net/images/extensions/'
-    };
-    
-    return {
-        get: function (file) {
-            file = file.replace(/data\/shared\//, '');
-            return (document.location.protocol === 'http:' ? config.endpoint.http : config.endpoint.https) + file;
-        }
-    };
-    
+var DataWrapper = function() {
+  return {
+    get: function(file) {
+      return safari.extension.baseURI + file;
+    }
+  };
 };
+
 if(!xt) var xt = {};
 xt.data = DataWrapper();
 
